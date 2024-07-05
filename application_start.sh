7c122b0cf93d75271052983f0cb8a7e53d6b1981
mmaazkhna <maazkotha007@gmail.com>
2024-07-06 00:17:10 +0500

    update files

--- application_start.sh
+++ application_start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Navigate to the deployment directory
-cd /home/ubuntu/nodejs-aws-codedeploy-pipeline
+# Change directory to the deployment directory
+cd /var/www/html
+
+# Install npm dependencies (if needed)
+# npm install
 
 # Start your Node.js application
 npm start
