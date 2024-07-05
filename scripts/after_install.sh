a366f016a488f2ebfec89dd0701129f7d29077e6
mmaazkhna <maazkotha007@gmail.com>
2024-07-05 23:21:57 +0500

    update files

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Navigate to the deployment directory
 cd /home/ubuntu/nodejs-aws-codedeploy-pipeline
 
-# Install npm dependencies
+Install npm dependencies
 npm install
 
-# Build your application (if necessary)
 npm run build
