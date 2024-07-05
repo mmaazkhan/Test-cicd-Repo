48df1264a28db75f4b9b011a3669307ac58cf0e1
mmaazkhna <maazkotha007@gmail.com>
2024-07-05 23:01:50 +0500

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'cd /home/ubuntu/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
 echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
