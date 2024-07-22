d7dcc2c3fce12882853bb6e5169d19ccedb17f73
mmaazkhan <93715851+mmaazkhan@users.noreply.github.com>
2024-07-22 16:02:03 +0500

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# SSH into the VM instance and pull the latest changes
-gcloud compute ssh test-instance --zone=us-central1-c --command "https://github.com/mmaazkhan/Test-cicd-Repo.git && git pull origin main && sudo systemctl restart nginx"
+# Variables
+INSTANCE_NAME="instance-001"
+ZONE="	us-central1-a	"
+PROJECT_ID="centered-kiln-428807-r4"
+REPO_PATH="https://github.com/mmaazkhan/Test-cicd-Repo.git"
+SERVICE_ACCOUNT_EMAIL="maazkotha007@gmail.com"
+
+# Authenticate with the service account
+gcloud auth activate-service-account $SERVICE_ACCOUNT_EMAIL --key-file=/path/to/your/service-account-key.json
+
+# SSH into the instance and pull the latest code
+gcloud compute ssh $INSTANCE_NAME --zone=$ZONE --command "cd $REPO_PATH && git pull origin main && sudo systemctl restart nginx"
